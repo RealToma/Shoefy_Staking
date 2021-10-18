4cc70c31a4f199c7fb67cdecab7533ea1235267f
cartoonweber <cartonweber0426@gmail.com>
2021-10-18 23:26:59 +0800

    add static staking

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
+import {createWeb3ReactRoot, Web3ReactProvider} from '@web3-react/core';
 
 // and bootstrap
 import 'bootstrap';
@@ -17,6 +18,7 @@ import { css } from 'styled-components';
 import {Shell} from './components/shell';
 import Dashboard from './components/pages/dashboard/dashboard';
 import StakingComponent from './components/pages/stakingComponent';
+import Staking2Component from './components/pages/staking2Component';
 import NFTStakingComponent from './components/pages/nftStakingComponent';
 
 import './i18n';
@@ -26,12 +28,18 @@ const pagesInNavigator = [
 	{ id: 'shoefyStaking', title: 'sNFT Staking', component: StakingComponent },
 	{ id: 'nftStaking', title: 'sNFT Farming', component: NFTStakingComponent},
 	{ id: 'snftStaking', title: '$Shoe Staking', component: NFTStakingComponent},
-
+	{ id: 'shoefyStaking2', title: 'Static $Shoe Staking', component: Staking2Component},
 ];
 
 const overrideCss = css`
 	margin-left: 50%
 `;
+
+// import {getLibrary} from './utils/getLibrary';
+
+// const NetworkContextName = `${new Date().getTime()}-NETWORK`;
+// const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName);
+// <Web3ReactProvider getLibrary={getLibrary}>
 
 // initialize modals
 Modal.setAppElement('#root');
@@ -39,6 +47,6 @@ Modal.setAppElement('#root');
 ReactDOM.render(
 	<React.Suspense fallback={<ClipLoader color={"#FFFFFF"} css={overrideCss}/>}>
 		<Shell pages={pagesInNavigator} />
-	</React.Suspense>,
-    document.getElementById('root')
+	</React.Suspense>
+	,document.getElementById('root')
 );
